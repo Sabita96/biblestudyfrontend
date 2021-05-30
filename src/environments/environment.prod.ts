5429a5c06567afe4193b67033ad918d5e9d6e429
sabita <sabita.nadar@cloudesign.com>
2021-05-30 22:30:30 +0530

    modified env file for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
-  production: true
+  production: true,
+};
+export const SharedEnvironment = {
+  apiUrl: "https://tamilbiblestudybackend.herokuapp.com/",
+
+  // apiUrl: 'https://test1.upskill.researcher.life/api/',
+  // stripeKey: 'pk_test_gsV0xMz9R860Xa8AiOpGjWBg00pj8zeQQf',
+  // JWT_TOKEN: '_id_test1',
+  // clevertapId: 'TEST-969-599-495Z',
+  // skillUrl: 'http://local.upskillweb.researcher.life',
+  // regUrl: 'http://test1.accounts.researcher.life/',
+  // enableWebPushNotification: false,
+  // paypalClientId: 'AaH8dI02DBuGMmpLy6-T9UIyJCV0di2NLMF1n0R3hdnE8eoW3p8fD8PvUHZW47xZ05p2NRUgVd8n3UEO',
+  // slackWbhook: 'https://hooks.slack.com/services/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',
+  // ampUrl: 'https://test1.upskill.researcher.life/amp',
+  // googlePixelId: 'AW-791333324',
+  // facebookPixelId: '199923051786407',
+  // twitterPixelId: 'o3lvq',
 };
