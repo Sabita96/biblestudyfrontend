c5c41127b117a6fc836e89f3182c944cd814ce2c
sabita <sabita.nadar@cloudesign.com>
2021-07-16 13:28:15 +0530

    Added code

--- src/app/services/download-service/download.service.ts
+++ src/app/services/download-service/download.service.ts
@@ -7,62 +7,28 @@ import { HttpService } from "../http-service/http.service";
 })
 export class DownloadService {
   constructor(private httpService: HttpService) {}
-  downloadNotes(subTopic) {
-    // const storage = getStorage();
-    // getDownloadURL(ref(storage, "images/stars.jpg"))
-    //   .then((url) => {
-    //     // `url` is the download URL for 'images/stars.jpg'
-
-    //     // This can be downloaded directly:
-    //     const xhr = new XMLHttpRequest();
-    //     xhr.responseType = "blob";
-    //     xhr.onload = (event) => {
-    //       const blob = xhr.response;
-    //     };
-    //     xhr.open("GET", url);
-    //     xhr.send();
-
-    //     // Or inserted into an <img> element
-    //     const img = document.getElementById("myimg");
-    //     img.setAttribute("src", url);
-    //   })
-    //   .catch((error) => {
-    //     // Handle any errors
-    //   });
-    // // Create a reference with an initial file path and name
-    // var storage = firebase.storage();
-    // var pathReference = storage.ref("images/stars.jpg");
-
-    // // Create a reference from a Google Cloud Storage URI
-    // var gsReference = storage.refFromURL("gs://bucket/images/stars.jpg");
-
-    // Create a reference from an HTTPS URL
-    // Note that in the URL, characters are URL escaped!
-    // var httpsReference = storage.refFromURL(
-    //   "https://firebasestorage.googleapis.com/b/bucket/o/images%20stars.jpg"
-    // );
-
-    // console.log("inside...............", subTopic.name);
-
-    // this.externalServicefileLink
-    //   .getInvestorAttachments(formID)
-    //   .toPromise()
-    // //   .then((res: any) => {
-    //     if (res.Value.length > 0) {
-    //       res.Value.forEach((element) => {
+  downloadPdf(subTopic) {
+   
     return this.httpService.get(
       subTopic.fileLink,
 
       { responseType: "arraybuffer" }
     );
 
-    // .catch((err) => {
-    //   console.log(err);
-    // });
-    //     });
-    //   } else {
-    //     this.toastr.error("Error While downloading pdf!!!");
-    //   }
-    // })
   }
+
+  downloadImage(imageUrl: string) {
+    return this.httpService.get(imageUrl, { responseType: 'blob' });
+    // return this.httpService.get(imageUrl, {observe: 'response', responseType: 'blob'})
+      
+  }
+  // downloadImage(img) {
+   
+  //   return this.httpService.get(
+  //     img,null
+
+  //     // { responseType: "arraybuffer" }
+  //   );
+
+  // }
 }
