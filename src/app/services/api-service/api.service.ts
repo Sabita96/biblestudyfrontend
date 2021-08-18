490ccfea43865b228b8be00f5e2b5410ae941f99
sabita <sabita.nadar@cloudesign.com>
2021-08-18 23:22:44 +0530

    .\

--- src/app/services/api-service/api.service.ts
+++ src/app/services/api-service/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { SharedEnvironment } from "environments/environment";
 
 @Injectable({
   providedIn: "root",
@@ -9,24 +10,34 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   get(url: string, param: any): Observable<any> {
-    return this.http.get(url, { params: param });
+    return this.http.get(`${SharedEnvironment.apiUrl}` + url, {
+      params: param,
+    });
   }
 
   post(url: string, body: Object | Array<Object>): Observable<any> {
-    return this.http.post(url, body);
+    console.log(
+      "`${SharedEnvironment.apiUrl}` + url",
+      `${SharedEnvironment.apiUrl}` + url
+    );
+
+    return this.http.post(`${SharedEnvironment.apiUrl}` + url, body);
   }
 
   put(url: string, body: Object | Array<Object>): Observable<any> {
-    return this.http.put(url, body);
+    return this.http.put(`${SharedEnvironment.apiUrl}` + url, body);
   }
   patch(url: string, body: Object | Array<Object>): Observable<any> {
-    return this.http.patch(url, body);
+    return this.http.patch(`${SharedEnvironment.apiUrl}` + url, body);
   }
 
   delete(url: string, options: any): Observable<any> {
     return this.http.delete(url, options);
   }
   getWithObserve(url: string, param: any): Observable<any> {
-    return this.http.get(url, { observe: "response", responseType: "text" });
+    return this.http.get(`${SharedEnvironment.apiUrl}` + url, {
+      observe: "response",
+      responseType: "text",
+    });
   }
 }
